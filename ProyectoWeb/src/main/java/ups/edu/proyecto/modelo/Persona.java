f4e94882f860657a427921236209843926c8718f
Bryan Méndez <47094533+BryanAndresMendezTenecora@users.noreply.github.com>
2021-07-19 16:26:15 -0500

    probandoCommit2

---
+++ ProyectoWeb/src/main/java/ups/edu/proyecto/modelo/Persona.java
@@ -0,0 +1,10 @@
+package ups.edu.proyecto.modelo;
+
+public class Persona {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola");
+	}
+
+}
