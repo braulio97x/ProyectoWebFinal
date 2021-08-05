9eecefd8202cafba331003b7d55f45abd829bcda
braulio97x <52545107+braulio97x@users.noreply.github.com>
2021-08-05 16:52:11 -0500

    iniciar sesion angular

--- ClienteAngular/proyecto/clienteVentas/src/app/app-routing.module.ts
+++ ClienteAngular/proyecto/clienteVentas/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IniciarSesionComponent } from './iniciar-sesion/iniciar-sesion.component';
+import { CrearCuentaComponent } from './crear-cuenta/crear-cuenta.component';
 
 const routes: Routes = [
-  {path: "iniciarSesion", component:IniciarSesionComponent}
+  {path: "iniciarSesion", component:IniciarSesionComponent},
+  {path: "crearCuenta", component: CrearCuentaComponent}
 ];
 
 @NgModule({
