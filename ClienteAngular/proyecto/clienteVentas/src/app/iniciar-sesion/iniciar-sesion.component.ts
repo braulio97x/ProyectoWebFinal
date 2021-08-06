80f4bc26536e7bc6452f4c374c91635654d4db55
braulio97x <52545107+braulio97x@users.noreply.github.com>
2021-08-05 23:26:05 -0500

    Pagina inicio de angular

--- ClienteAngular/proyecto/clienteVentas/src/app/iniciar-sesion/iniciar-sesion.component.ts
+++ ClienteAngular/proyecto/clienteVentas/src/app/iniciar-sesion/iniciar-sesion.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
 import { NavigationExtras, Router } from '@angular/router';
 import { Sesion } from '../domain/sesion';
 import { SesionesService } from '../services/sesiones.service';
@@ -13,8 +13,8 @@ import { SesionesService } from '../services/sesiones.service';
  
 })
 export class IniciarSesionComponent implements OnInit {
- // @Input() visible: boolean;
-//Output close:EventEmitter<boolean> =new EventEmitter;
+
+//@Input() visible:boolean;
 @Output() close:EventEmitter<boolean>=new EventEmitter();
   usuario: string= "Braulio Astudillo";
   sesion:Sesion=new Sesion();
